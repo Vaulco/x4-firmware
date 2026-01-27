b615c3ef57b326833f43341f538048b508d888b2
Vaulco <dniemtsov@gmail.com>
2026-01-28 08:25:02 +1100

    fixed setting count

--- src/activities/settings/SettingsActivity.cpp
+++ src/activities/settings/SettingsActivity.cpp
@@ -10,7 +10,7 @@
 
 // Define the static settings list
 namespace {
-constexpr int settingsCount = 15;
+constexpr int settingsCount = 6;
 const SettingInfo settingsList[settingsCount] = {
     // Should match with SLEEP_SCREEN_MODE
     SettingInfo::Enum("Sleep Screen", &CrossPointSettings::sleepScreen, {"Dark", "Light", "Custom", "Cover", "None"}),
