9013b0d9b6007f98e8f005bca2bc7ac86ca1ab29
Vaulco <dniemtsov@gmail.com>
2026-02-16 08:59:54 +1100

    updated file selection to use selection activity

--- src/activities/reader/FileSelectionActivity.cpp
+++ src/activities/reader/FileSelectionActivity.cpp
@@ -1,15 +1,7 @@
 #include "FileSelectionActivity.h"
 
-#include <GfxRenderer.h>
 #include <SDCardManager.h>
 #include <Xtc/XtcTypes.h>
-
-#include "Battery.h"
-
-namespace {
-constexpr int PAGE_ITEMS = 23;
-constexpr int SKIP_PAGE_MS = 700;
-}  // namespace
 
 void sortFileList(std::vector<std::string>& strs) {
   std::sort(begin(strs), end(strs), [](const std::string& str1, const std::string& str2) {
@@ -21,14 +13,9 @@ void sortFileList(std::vector<std::string>& strs) {
   });
 }
 
-void FileSelectionActivity::taskTrampoline(void* param) {
-  auto* self = static_cast<FileSelectionActivity*>(param);
-  self->displayTaskLoop();
-}
-
 void FileSelectionActivity::loadFiles() {
   files.clear();
-  selectorIndex = 0;
+  selectedIndex = 0;
 
   auto root = SdMan.open(basepath.c_str());
   if (!root || !root.isDirectory()) {
@@ -55,130 +42,4 @@ void FileSelectionActivity::loadFiles() {
   }
   root.close();
   sortFileList(files);
-}
-
-void FileSelectionActivity::onEnter() {
-  Activity::onEnter();
-
-  renderingMutex = xSemaphoreCreateMutex();
-
-  // basepath is set via constructor parameter (defaults to "/" if not specified)
-  loadFiles();
-  selectorIndex = 0;
-
-  // Trigger first update
-  updateRequired = true;
-
-  xTaskCreate(&FileSelectionActivity::taskTrampoline, "FileSelectionActivityTask",
-              2048,               // Stack size
-              this,               // Parameters
-              1,                  // Priority
-              &displayTaskHandle  // Task handle
-  );
-}
-
-void FileSelectionActivity::onExit() {
-  Activity::onExit();
-
-  // Wait until not rendering to delete task to avoid killing mid-instruction to EPD
-  xSemaphoreTake(renderingMutex, portMAX_DELAY);
-  if (displayTaskHandle) {
-    vTaskDelete(displayTaskHandle);
-    displayTaskHandle = nullptr;
-  }
-  vSemaphoreDelete(renderingMutex);
-  renderingMutex = nullptr;
-  files.clear();
-}
-
-void FileSelectionActivity::loop() {
-  // Back button: go up one directory, or to Settings if at root
-  if (inputManager.wasReleased(InputManager::Button::Back)) {
-    if (basepath != "/") {
-      // Go up one directory
-      basepath.replace(basepath.find_last_of('/'), std::string::npos, "");
-      if (basepath.empty()) basepath = "/";
-      loadFiles();
-      updateRequired = true;
-    } else {
-      // At root - go to Settings
-      onGoToSettings();
-    }
-    return;
-  }
-
-  const bool prevReleased = inputManager.wasReleased(InputManager::Button::Up) ||
-                            inputManager.wasReleased(InputManager::Button::Left);
-  const bool nextReleased = inputManager.wasReleased(InputManager::Button::Down) ||
-                            inputManager.wasReleased(InputManager::Button::Right);
-
-  const bool skipPage = inputManager.getHeldTime() > SKIP_PAGE_MS;
-
-  if (inputManager.wasReleased(InputManager::Button::Confirm)) {
-    if (files.empty()) {
-      return;
-    }
-
-    if (basepath.back() != '/') basepath += "/";
-    if (files[selectorIndex].back() == '/') {
-      basepath += files[selectorIndex].substr(0, files[selectorIndex].length() - 1);
-      loadFiles();
-      updateRequired = true;
-    } else {
-      onSelect(basepath + files[selectorIndex]);
-    }
-  } else if (prevReleased) {
-    if (skipPage) {
-      selectorIndex = ((selectorIndex / PAGE_ITEMS - 1) * PAGE_ITEMS + files.size()) % files.size();
-    } else {
-      selectorIndex = (selectorIndex + files.size() - 1) % files.size();
-    }
-    updateRequired = true;
-  } else if (nextReleased) {
-    if (skipPage) {
-      selectorIndex = ((selectorIndex / PAGE_ITEMS + 1) * PAGE_ITEMS) % files.size();
-    } else {
-      selectorIndex = (selectorIndex + 1) % files.size();
-    }
-    updateRequired = true;
-  }
-}
-
-void FileSelectionActivity::displayTaskLoop() {
-  while (true) {
-    if (updateRequired) {
-      updateRequired = false;
-      xSemaphoreTake(renderingMutex, portMAX_DELAY);
-      render();
-      xSemaphoreGive(renderingMutex);
-    }
-    vTaskDelay(10 / portTICK_PERIOD_MS);
-  }
-}
-
-void FileSelectionActivity::render() const {
-  renderer.clearScreen();
-
-  const auto pageWidth = renderer.getScreenWidth();
-  const auto pageHeight = renderer.getScreenHeight();
-
-  renderer.drawCenteredText(GfxRenderer::LARGE, 15, "Library", true);
-
-  if (files.empty()) {
-    renderer.drawText(GfxRenderer::MEDIUM, 20, 60, "No books found");
-  } else {
-    const auto pageStartIndex = selectorIndex / PAGE_ITEMS * PAGE_ITEMS;
-    renderer.fillRect(0, 60 + (selectorIndex % PAGE_ITEMS) * 30 - 2, pageWidth - 1, 30);
-    for (int i = pageStartIndex; i < files.size() && i < pageStartIndex + PAGE_ITEMS; i++) {
-      auto item = renderer.truncatedText(GfxRenderer::MEDIUM, files[i].c_str(), renderer.getScreenWidth() - 40);
-      renderer.drawText(GfxRenderer::MEDIUM, 20, 60 + (i % PAGE_ITEMS) * 30, item.c_str(), i != selectorIndex);
-    }
-  }
-
-  // New code - text-only battery percentage
-  const uint16_t batteryPercentage = battery.readPercentage();
-  const std::string batteryText = std::to_string(batteryPercentage) + "%";
-  renderer.drawCenteredText(GfxRenderer::SMALL, pageHeight - 30, batteryText.c_str());
-
-  renderer.displayBuffer();
 }
--- src/activities/reader/FileSelectionActivity.h
+++ src/activities/reader/FileSelectionActivity.h
@@ -1,38 +1,73 @@
 #pragma once
-#include <freertos/FreeRTOS.h>
-#include <freertos/semphr.h>
-#include <freertos/task.h>
-
 #include <functional>
 #include <string>
 #include <vector>
+#include <GfxRenderer.h>
 
-#include "../Activity.h"
+#include "activities/util/SelectionActivity.h"
 
-class FileSelectionActivity final : public Activity {
-  TaskHandle_t displayTaskHandle = nullptr;
-  SemaphoreHandle_t renderingMutex = nullptr;
+class FileSelectionActivity final : public SelectionActivity {
   std::string basepath = "/";
   std::vector<std::string> files;
-  int selectorIndex = 0;
-  bool updateRequired = false;
   const std::function<void(const std::string&)> onSelect;
   const std::function<void()> onGoToSettings;
 
-  static void taskTrampoline(void* param);
-  [[noreturn]] void displayTaskLoop();
-  void render() const;
   void loadFiles();
+
+ protected:
+  // SelectionActivity overrides
+  int getItemCount() const override { return static_cast<int>(files.size()); }
+  
+  void renderItem(int index, int x, int y, bool isSelected) const override {
+    auto item = renderer.truncatedText(GfxRenderer::MEDIUM, files[index].c_str(), 
+                                      renderer.getScreenWidth() - 40);
+    renderer.drawText(GfxRenderer::MEDIUM, x, y, item.c_str(), !isSelected);
+  }
+
+  void onItemSelected(int index) override {
+    if (files.empty()) {
+      return;
+    }
+
+    if (basepath.back() != '/') basepath += "/";
+    if (files[index].back() == '/') {
+      // Navigate into directory
+      basepath += files[index].substr(0, files[index].length() - 1);
+      loadFiles();
+      selectedIndex = 0;
+      updateRequired = true;
+    } else {
+      // Select file
+      onSelect(basepath + files[index]);
+    }
+  }
+
+  void onBack() override {
+    if (basepath != "/") {
+      // Go up one directory
+      basepath.replace(basepath.find_last_of('/'), std::string::npos, "");
+      if (basepath.empty()) basepath = "/";
+      loadFiles();
+      selectedIndex = 0;
+      updateRequired = true;
+    } else {
+      // At root - go to Settings
+      onGoToSettings();
+    }
+  }
 
  public:
   explicit FileSelectionActivity(GfxRenderer& renderer, InputManager& inputManager,
                                  const std::function<void(const std::string&)>& onSelect,
-                                 const std::function<void()>& onGoToSettings, std::string initialPath = "/")
-      : Activity("FileSelection", renderer, inputManager),
+                                 const std::function<void()>& onGoToSettings,
+                                 std::string initialPath = "/")
+      : SelectionActivity("FileSelection", "Library", renderer, inputManager),
         basepath(initialPath.empty() ? "/" : std::move(initialPath)),
         onSelect(onSelect),
         onGoToSettings(onGoToSettings) {}
-  void onEnter() override;
-  void onExit() override;
-  void loop() override;
+
+  void onEnter() override {
+    loadFiles();
+    SelectionActivity::onEnter();
+  }
 };
