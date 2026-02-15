837b583517f232f96fc07a6e97487121e3f42aff
Vaulco <dniemtsov@gmail.com>
2026-02-15 17:26:25 +1100

    combined fullscreenmessage files

--- src/activities/util/FullScreenMessageActivity.h
+++ src/activities/util/FullScreenMessageActivity.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <EInkDisplay.h>
-
 #include <string>
 #include <utility>
-
 #include "../Activity.h"
+#include "GfxRenderer.h"
+#include "fontIds.h"
 
 class FullScreenMessageActivity final : public Activity {
   std::string text;
@@ -16,5 +16,15 @@ class FullScreenMessageActivity final : public Activity {
       : Activity("FullScreenMessage", renderer, inputManager),
         text(std::move(text)),
         refreshMode(refreshMode) {}
-  void onEnter() override;
+  
+  void onEnter() override {
+    Activity::onEnter();
+
+    const auto height = renderer.getLineHeight(CMU_10_FONT_ID);
+    const auto top = (renderer.getScreenHeight() - height) / 2;
+
+    renderer.clearScreen();
+    renderer.drawCenteredText(CMU_10_FONT_ID, top, text.c_str(), true);
+    renderer.displayBuffer(refreshMode);
+  }
 };
