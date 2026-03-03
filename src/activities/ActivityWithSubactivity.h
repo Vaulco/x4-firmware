1c9dee967f4471009d57c62335263f27224a344a
Vaulco <dniemtsov@gmail.com>
2026-03-03 13:18:32 +1100

    combined activity with subactiviy into one .h

--- src/activities/ActivityWithSubactivity.h
+++ src/activities/ActivityWithSubactivity.h
@@ -1,17 +1,33 @@
 #pragma once
 #include <memory>
-
 #include "Activity.h"
 
 class ActivityWithSubactivity : public Activity {
  protected:
   std::unique_ptr<Activity> subActivity = nullptr;
-  void exitActivity();
-  void enterNewActivity(Activity* activity);
+
+  void exitActivity() {
+    if (subActivity) {
+      subActivity->onExit();
+      subActivity.reset();
+    }
+  }
+
+  void enterNewActivity(Activity* activity) {
+    subActivity.reset(activity);
+    subActivity->onEnter();
+  }
 
  public:
   explicit ActivityWithSubactivity(std::string name, GfxRenderer& renderer, InputManager& inputManager)
       : Activity(std::move(name), renderer, inputManager) {}
-  void loop() override;
-  void onExit() override;
+
+  void loop() override {
+    if (subActivity) subActivity->loop();
+  }
+
+  void onExit() override {
+    Activity::onExit();
+    exitActivity();
+  }
 };
